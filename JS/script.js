01ab03f0791d64fedb229f749643dd7dae017cd5
slowfarmtw <slowfarm.tw@gmail.com>
2026-06-28 14:28:55 +0800

    Update and rename script_V2.js to script.js

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.getElementById("menuToggle");
+const siteNav = document.getElementById("siteNav");
+
+if (menuToggle && siteNav) {
+  menuToggle.addEventListener("click", function () {
+    siteNav.classList.toggle("active");
+  });
+}
