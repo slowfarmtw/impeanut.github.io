f8fedfabfcf1c91d97b7d2a40317aec5c0459c42
slowfarmtw <slowfarm.tw@gmail.com>
2026-07-03 21:07:56 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwDQGYs_TYOhfeqClDZgPDokqUbMzpv1Zk2wAJJGe_ryi5AHhKFfTHDh_mbm4tuBbbJ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzqSpDO_xCy_EZ6afJtT5eTkfuQKyYU4SmRNmdkQx9IMke7_o-sQUPvcM2vbar3oR0t/exec";
 const ORDER_API_URL = API_URL;
